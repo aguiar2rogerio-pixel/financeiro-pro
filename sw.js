5a95d6eaa785c5a55b4484e92491817e2a864046
aguiar2rogerio-pixel <aguiar2.rogerio@gmail.com>
2026-05-23 01:47:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,11 @@
-const CACHE_NAME = 'financeiro-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json'
-];
+const CACHE_NAME = 'financas-v1';
+const ASSETS = ['./', './index.html'];
 
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-    );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(self.clients.claim());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
-    );
-});
-
