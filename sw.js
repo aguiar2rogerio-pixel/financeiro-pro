bc9ab4504dcfe97cb03c8b26f2ff1b7950812f62
aguiar2rogerio-pixel <aguiar2.rogerio@gmail.com>
2026-05-22 23:56:47 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,15 +2,24 @@ const CACHE_NAME = 'financeiro-v1';
 const assets = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/2454/2454261.png' // 👉 Adicionado o ícone ao cache
 ];
 
 self.addEventListener('install', e => {
+  // Força o Service Worker atual a se tornar ativo imediatamente, sem esperar o usuário fechar o app
+  self.skipWaiting(); 
+  
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
     })
   );
+});
+
+self.addEventListener('activate', e => {
+  // Permite que o Service Worker controle a página atual imediatamente após a ativação
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', e => {
